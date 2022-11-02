4967cfa476f2ab2cdc4e36c96410d0955010b60b
Riccardo Venturini <riccardo@fornacestudio.com>
2022-11-02 09:51:21 +0100

    user resource

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::factory([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin')
+        ])->create();
+        \App\Models\User::factory([
+            'name' => 'Foo',
+            'email' => 'bar@bar.com',
+        ])->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
